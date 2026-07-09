3ca51ca5d592763ca430aff1963890b245b00941
GabrielRezende079 <grezendabarbosaflorencio@gmail.com>
2026-07-08 21:52:28 -0300

    allwoend cors

--- siao-api/src/main.ts
+++ siao-api/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://alunos-de-siao.vercel.app/'], // Replace with your exact domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // If your app requires cookies or authorization headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
